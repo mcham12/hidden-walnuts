f11dbf69efb1046fa3beddf7478c41bb297c82e4
mcham12 <mattmcarroll@me.com>
2025-05-02 17:56:35 -0500

    Fix categories syntax error in config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ const siteConfig = {
             title: 'Celebrating Fine Art',
             image: 'hero-celebrating-fine-art.png',
             url: 'https://www.teepublic.com/user/hidden-walnuts?query=celebrating%20fine%20art'
-        }
+        },
         //{
         //    id: 'Pickleball',
-            title: 'Pickleball',
-            image: 'hero-pickleball.png',
-            url: 'https://www.teepublic.com/user/hidden-walnuts?query=pickleball'
-        }
+         //   title: 'Pickleball',
+          //  image: 'hero-pickleball.png',
+           // url: 'https://www.teepublic.com/user/hidden-walnuts?query=pickleball'
+       // }
         // Add more categories as needed
     ],
     
