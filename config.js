d8d5c43aef92be776c9d8c5b2a7aa29a87e59471
mcham12 <mattmcarroll@me.com>
2025-05-02 17:15:23 -0500

    updated categories

--- config.js
+++ config.js
@@ -13,7 +13,13 @@ const siteConfig = {
     // Categories
     categories: [
         {
-            id: 'Pickleball',
+            id: 'Celebrating Fine Art',
+            title: 'Celebrating Fine Art',
+            image: 'hero-celebrating-fine-art.png',
+            url: 'https://www.teepublic.com/user/hidden-walnuts?query=celebrating%20fine%20art'
+        },
+        //{
+        //    id: 'Pickleball',
             title: 'Pickleball',
             image: 'hero-pickleball.png',
             url: 'https://www.teepublic.com/user/hidden-walnuts?query=pickleball'
